9abbed188eb72baf2f607bfaa8ac40767438e0c1
Zuhayr Khan <zuhayrkhan@Zuhayrs-Air.lan>
2023-05-19 14:26:32 +0100

    17 tests passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -49,3 +49,6 @@ def remove_customer_cash(customer_cash, amount):
 
 def get_customer_pet_count(customer_pet):
     return len(customer_pet['pets'])
+
+def add_pet_to_customer(customer, new_pet):
+    customer['pets'].append(new_pet)
